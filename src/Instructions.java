20d58243698d8bf40887f6e61949eab805eba259
brucethao <thao575@uwm.edu>
2025-04-13 19:36:18 -0500

    completed I-Format

--- src/Instructions.java
+++ src/Instructions.java
@@ -13,16 +13,57 @@ public class Instructions {
     }
 
     //Parses and prints R-type instruction
-    public static void rType(int opcode, int binary) {
-
+    //mnemonic {opcode: XX, rs: XX, rt: XX, rd: XX, shmt: XX, funct: XX}
+    public static void rType(int opcode, int funct, int binary) {
+        String mnemonic;
+        switch(funct){
+            case 32 -> mnemonic = "add"; //32 is the decimal of 0x20
+            case 36 -> mnemonic = "and"; //0x24
+            case 37 -> mnemonic = "or"; //0x25
+            case 42 -> mnemonic = "slt"; //0x2A
+            case 34 -> mnemonic = "sub"; //0x22
+            default -> {
+                System.out.println("Unknown R-type funct: " + funct);
+                return;
+            }
+        }
+        //shift
+        int rs = (binary >>> 21) & 0x1F;
+        int rt = (binary >>> 16) & 0x1F;
+        int rd = (binary >>> 11) & 0x1F;
+        int shmt = (binary >>> 6) & 0x1F;
+        String output = String.format(mnemonic+" {opcode: %02x, rs: %02x, rt: %02x, rd: %02x, shmt: %02x, funct: %02x}"
+                , opcode, rs, rt, rd, shmt, funct);
+        System.out.println(output);
     }
 
     //Parses and prints I-type instruction
+    //mnemonic {opcode: XX, rs(base): XX, rt: XX, immediate(offset): XXXX}
     public static void iType(int opcode, int binary) {
-
+        String mnemonic;
+        switch(opcode){
+            case 9 -> mnemonic = "addiu";// opcode = 0x09
+            case 12 -> mnemonic = "andi";// opcode = 0x0C
+            case 4 -> mnemonic = "beq"; // opcode = 0x04
+            case 5 -> mnemonic = "bne";// opcode = 0x05
+            case 15 -> mnemonic = "lui";// opcode = 0x0F
+            case 35 -> mnemonic = "lw";// opcode = 0x23
+            case 13 -> mnemonic = "ori";// opcode = 0x0D
+            case 43 -> mnemonic = "sw";// opcode = 0x2B
+            default -> {
+                System.out.println("Unknown I-type op: " + opcode);
+                return;
+            }
+        }
+        int rsBase = (binary>>21) & 0x1F;
+        int rt = (binary>>16) & 0x1F;
+        int immediateOffset = binary & 0x1FF; // mask - only want lower 16 bits
+        String output = String.format(mnemonic+" {opcode: %02x, rs(base): %02x, rt: %02x, immediate(offset): %04x}", opcode, rsBase, rt, immediateOffset);
+        System.out.println(output);
     }
 
     //Parses and prints J-type instruction
+    //mnemonic {opcode: XX, index: XXXXXXX}
     public static void jType(int opcode, int binary) {
         int index = binary & 0x3FFFFFF;
         String output = String.format("j {opcode: %02x, index: %07x}", opcode, index);
@@ -30,6 +71,7 @@ public class Instructions {
     }
 
     //print syscall instruction
+    //mnemonic {opcode: XX, code: 000000, funct: XX}
     public static void syscall() {
         String output = "syscall {opcode: 00, code: 000000, funct: 0c}";
         System.out.println(output);
--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,35 @@
 public class Main {
     public static void main(String[] args) {
         //String input = args[0];
-        //String input = "020ed020"; // rType
+        //String input = "02f43824"; // rType
         //String input = "246600e0"; // iType
-        String input = "080000c7"; // jType
+        //String input = "080000c7"; // jType
         //String input = "0000000c"; //syscall
+
+        //String input = "274700c3"; //iType addiu
+        //String input = "32110039"; // iType andi
+        //String input = "10c900cd"; // iType beq
+        //String input = "16cb00aa"; // iType bne
+        //String input = "3c1d0071"; // iType lui
+        //String input = "8fb20000"; // iType lw
+        //String input = "3413ff20"; // iType ori
+        String input = "ac0d0000"; //iType sw
 
         int binary = (int) Long.parseLong(input, 16);
 
-        // & 0x3F is used to mask out and extract only the lowest 6 bits
+// & 0x3F is used to mask out and extract only the lowest 6 bits
         int opcode = (binary >>> 26) & 0x3F;
         int funct = binary & 0x3F; // for distinguishing syscall from other R-types
         String type = Instructions.InstructionType(opcode, funct);
 
-        System.out.println(type);
+        //System.out.println(type);
 
         switch (type) {
-            case "R" -> Instructions.rType(opcode, binary);
+
+            case "R" -> Instructions.rType(opcode, funct, binary);
             case "I" -> Instructions.iType(opcode, binary);
             case "J" -> Instructions.jType(opcode, binary);
             default -> Instructions.syscall();
         }
     }
 }
-
